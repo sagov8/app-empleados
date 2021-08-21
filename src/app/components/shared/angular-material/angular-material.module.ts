287ebc3a57bca4b3eeeb078a42f80cd946fea063
sagov8 <sago.vivas@gmail.com>
2021-08-21 13:01:40 -0500

    Navbar component

--- src/app/components/shared/angular-material/angular-material.module.ts
+++ src/app/components/shared/angular-material/angular-material.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSliderModule } from '@angular/material/slider';
-
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatSliderModule
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule
   ],
   exports: [
-    MatSliderModule
+    MatSliderModule,
+    MatToolbarModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule
   ]
 })
 export class AngularMaterialModule { }
